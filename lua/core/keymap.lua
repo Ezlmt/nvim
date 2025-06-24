544e45bb29ca1b3bb8ba7ee1727333e2e8162126
Ezlmt <2876187491@qq.com>
2025-06-24 14:49:58 +0800

    add bufferline, basic keymap catppuccin

---
+++ lua/core/keymap.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("i", "jk", "<Esc>" , { silent = true})
---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function ()
+    vim.opt.termguicolors = true
+    require("catppuccin").setup({
+      flavour = "mocha",
+      transparent_background = true,
+      
+    })
+    vim.cmd("colorscheme catppuccin")
+  end
+}
