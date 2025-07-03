6d7d5370183443830d771946aab5b539a2c78fc7
Ezlmt <2876187491@qq.com>
2025-07-03 19:29:57 +0800

    image & init
    1. unable image-nvim when use neovide
    2. set the color of the Line, which brighter that before

--- init.lua
+++ init.lua
@@ -3,3 +3,6 @@ require "daps.init"
 
 vim.lsp.enable "lua_ls"
 vim.lsp.enable "clangd"
+
+vim.api.nvim_set_hl(0, "LineNrAbove",   { fg = "#7f849c" })
+vim.api.nvim_set_hl(0, "LineNrBelow",   { fg = "#7f849c" })
--- lua/plugins/image-nvim.lua
+++ lua/plugins/image-nvim.lua
@@ -9,6 +9,14 @@ return {
 	{
 		"3rd/image.nvim",
 		dependencies = { "luarocks.nvim" },
+    cond = function()
+      if vim.g.neovide or vim.g.goneovim or vim.g.GuiLoaded then
+        return false
+      end
+      local ui_ok  = #vim.api.nvim_list_uis() > 0
+      local term   = os.getenv("TERM") or ""
+      return ui_ok and (term:match("kitty") or term:match("wezterm"))
+    end,
 		opts = {},
 	},
 }
