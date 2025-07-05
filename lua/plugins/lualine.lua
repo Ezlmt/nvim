15251a460f34b85ac0223dd4e123fcd6fd6d384a
Ezlmt <2876187491@qq.com>
2025-07-05 19:11:31 +0800

    update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,23 +1,28 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    event = "VeryLazy",
-    opts = {
-        options = {
-            theme = "auto",
-            component_separators = { left = "", right = "" },
-            section_separators = { left = "", right = "" },
-        },
-        extensions = { "nvim-tree" },
-        sections = {
-            lualine_b = { "branch", "diff" },
-            lualine_x = {
-                "filesize",
-                "encoding",
-                "filetype",
-            },
-        },
-    },
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	event = "VeryLazy",
+	opts = {
+		options = {
+			theme = "auto",
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+		},
+		extensions = { "nvim-tree" },
+		sections = {
+			lualine_b = { "branch", "diff" },
+			lualine_x = {
+				"filesize",
+				"lsp_status",
+			},
+			lualine_y = {
+				"encoding",
+				"fileformat",
+				"filetype",
+				"progress",
+			},
+		},
+	},
 }
