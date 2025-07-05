7fe701dbc998e9bf8536b43c338b7e18782d0412
Ezlmt <2876187491@qq.com>
2025-07-05 15:44:15 +0800

    new motion key in insert model

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -1,4 +1,10 @@
+local map = vim.keymap.set
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
-vim.keymap.set("i", "jk", "<Esc>" , { silent = true})
+map("i", "jk", "<Esc>" , { silent = true})
+
+map("i", "<C-h>", "<Left>")
+map("i", "<C-j>", "<Down>")
+map("i", "<C-k>", "<Up>")
+map("i", "<C-l>", "<Right>")
