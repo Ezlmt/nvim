99440299a3d30f1cc43d57026be5f5d12813f541
Ezlmt <2876187491@qq.com>
2025-07-05 15:57:45 +0800

    catppuccin
    update MatchParen's color to adapt rainbow-delimiter

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -8,6 +8,11 @@ return {
       flavour = "mocha",
       transparent_background = true,
       term_colors = true,
+      custom_highlights = function (colors)
+        return {
+          MatchParen = { bg = colors.lavender, fg = colors.base, bold = true},
+        }
+      end,
       integrations = {
         alpha = true,
         cmp = true,
@@ -16,6 +21,7 @@ return {
         nvimtree = true,
         telescope = true,
         notify = true,
+        rainbow_delimiters = true,
       },
     })
     vim.cmd("colorscheme catppuccin")
